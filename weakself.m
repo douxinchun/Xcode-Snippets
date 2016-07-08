611fc85bd5156a3e4722695a0705993065556a17
xinchundou <xinchundou@sohu-inc.com>
2016-07-08 13:16:59 +0800

    Imported user code snippets

---
+++ weakself.m
@@ -0,0 +1,10 @@
+// weakSelf
+// 
+//
+// IDECodeSnippetCompletionPrefix: ws
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 8B414E49-A64B-4EB4-95A7-5DB1AB94EE32
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+ __weak typeof(self) weakSelf = self;
